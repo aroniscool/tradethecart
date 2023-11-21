c4da957fadc5ce95ff0ad8cbd08bcebc1fcc598a
aroniscool <arien.bhuiyan1@gmail.com>
2023-11-21 23:24:40 +0000

    progress

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = 3000;
+const mysql = require('mysql2');
 app.use(express.static(path.join(__dirname, "./public")));
 app.set('view engine', 'ejs');
 
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    database:"abhuiyan01",
+    password:"",
+    port:"3306",
+});
+
 app.get('/', (req, res) => {
-    res.render('index')
+    let setsql = `SELECT * FROM ttc_sets`;
+    db.query(setsql, (err, result) => {
+        if (err) throw err;
+        res.render('index', { sets : result });
+    });
 });
 
 app.listen(PORT, () => {
