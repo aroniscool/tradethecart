58139c9f080336c82af3959696fc4aacde60c76c
aroniscool <arien.bhuiyan1@gmail.com>
2023-11-22 17:08:35 +0000

    Login startups

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const app = express();
 const path = require("path");
 const PORT = 3000;
 const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const sessions = require('express-session');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "./public")));
 app.set('view engine', 'ejs');
+app.use(cookieParser());
 
 const db = mysql.createConnection({
     host:"localhost",
@@ -21,6 +26,31 @@ app.get('/', (req, res) => {
         res.render('index', { sets : result });
     });
 });
+
+app.get('/login', (req, res) => {
+    res.render('login')
+});
+
+/*
+app.post('/login', (req,res) => {
+    const useremail = req.body.emailField;
+    const checkuser = `SELECT * FROM ttc_users WHERE email = "${useremail}" `;
+
+    db.query(checkuser, (err, rows) => {
+        if(err) throw err;
+        const numRows = rows.length;
+        
+        if(numRows > 0){
+            const sessionobj = req.session;  
+            sessionobj.authen = rows[0].id; 
+            res.redirect('/');
+        }else{
+            res.redirect('/login');
+        }
+        
+    });
+});
+*/
 
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`)
